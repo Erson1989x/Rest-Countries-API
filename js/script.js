a99c8e8a5e04bfd1587c15ece899c950729568d2
Erhard Nagy <Erson1989x@gmail.com>
2024-11-28 19:48:56 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-import initDarkMode from "/rest-countries-api/js/darkMode.js";
-import initFilter from "/rest-countries-api/js/filter.js";
+import initDarkMode from "/js/darkMode.js";
+import initFilter from "/js/filter.js";
 
 const countriesContainer = document.getElementById('countries-container');
 const searchInput = document.getElementById('search-input');
